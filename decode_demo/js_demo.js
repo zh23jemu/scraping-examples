3d1ad586b940ee37236fd4ee8a13d00851bbdc8a
ZH23J_Emu <231016896@qq.com>
2025-01-24 14:15:11 +0800

    Add js_demo

---
+++ decode_demo/js_demo.js
@@ -0,0 +1,4 @@
+// JavaScript 示例代码
+function add(a, b) {
+    return a + b;
+}
---
+++ decode_demo/js_demo.py
@@ -0,0 +1,7 @@
+import execjs  # pip install PyExecJS
+
+with open('js_demo.js', 'r', encoding='UTF-8') as file:
+    js_code = file.read()
+
+ctx = execjs.compile(js_code)
+print(ctx.call('add', 3, 5))
